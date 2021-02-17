a0dd9f52b21aed2fe135087984b2a73081e23590
Joseph Thompson <joe.thompson838@gmail.com>
2021-02-16 22:40:10 -0500

    updated databases for postgresql

--- btrs/settings.py
+++ btrs/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'btrs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'joseph',
+        'PASSWORD': 'BitcoinExpress838',
+        'HOST': 'localhost'
     }
 }
 
