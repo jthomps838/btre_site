61b80de3f04e6c1c14cd1778ee999d9096f7560d
Joseph Thompson <joe.thompson838@gmail.com>
2021-02-18 14:46:31 -0500

    add email functionality, no password given

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 # Create your views here.
 
@@ -15,10 +16,30 @@ def contacts(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        if request.user.is_authenticated:
+            user_id = request.user.id
+
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
+
+            if has_contacted:
+                messages.error(
+                    request, 'You have already made an inquiry on this property.')
+                return redirect('/listings/' + listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name,
                           email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
+
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for ' + listing +
+            '. Sign into the admin panel for more info',
+            'joe.thompson@gmail.com',
+            [realtor_email, 'joseph@josephmt.com'],
+            fail_silently=False
+        )
 
         messages.success(
             request, 'Your request has been submitted, a realtor will get back to you soon.')
